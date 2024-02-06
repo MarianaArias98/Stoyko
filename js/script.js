b18db3757c022ecb1b223aff20252130a278689b
MarianaArias98 <marian.ma887@gmail.com>
2024-02-06 01:51:49 -0300

    Cambios index.html css & js

--- js/script.js
+++ js/script.js
@@ -169,3 +169,78 @@ myVideo();
 
 
 
+
+//FUNCIONAMIENTO DEL FORMULARIO  DE CONTACTO
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.getElementById('myForm');
+    var submitButton = document.getElementById('submit_button');
+    var validationMessages = document.getElementById('validationMessages');
+
+    // Función para generar dos números aleatorios y mostrar la pregunta del captcha
+    function generarCaptcha() {
+        var num1 = Math.floor(Math.random() * 10);
+        var num2 = Math.floor(Math.random() * 10);
+        var suma = num1 + num2;
+
+        document.getElementById("captcha-question").textContent = num1 + " + " + num2 +  " = "; // Mostrar la pregunta en el elemento span
+
+        document.getElementById("captcha").dataset.correctSum = suma; // Establecer el valor correcto en el campo de entrada
+        // Validar el captcha después de generar
+        validateCaptcha();
+    }
+    window.onload = generarCaptcha; // Llamar a la función al cargar la página
+
+    // Función para validar el formulario y el captcha
+
+    submitButton.addEventListener('click', function (event) {
+        var inputs = form.querySelectorAll('input:required, textarea:required, #captcha'); // Agregar el campo del captcha al selector
+        var isValid = true;
+    
+        inputs.forEach(function (input) {
+            if (input.value.trim() === '') {
+                isValid = false;
+                input.style.border = '2px solid #059b4d';
+            } else {
+                input.style.border = '1px solid #ccc';
+            }
+        });
+
+    
+        if (!isValid) {
+            event.preventDefault(); // Evitar que el formulario se envíe si hay campos vacíos
+    
+            // Crear el mensaje de validación con elementos de lista
+            var validationMessage = '<ul class="validation-list">';
+            inputs.forEach(function (input) {
+                if (input.value.trim() === '') {
+                    validationMessage += '<li class="validation-item">' + input.getAttribute('placeholder') + '</li>';
+                }
+            });
+            validationMessage += '</ul>';
+            
+            // Insertar el mensaje de validación debajo del h2
+            validationMessages.innerHTML = '<p class="validation-intro">Por favor complete los siguientes campos:</p>' + validationMessage;
+        } else {
+            validationMessages.innerHTML = ''; // Limpiar el mensaje de validación
+            var isValidCaptcha = validateCaptcha();
+            if (!isValidCaptcha) {
+                event.preventDefault(); // Evitar que el formulario se envíe si el captcha es incorrecto
+                displayCaptchaError(); // Mostrar mensaje de error del captcha
+            }
+        }
+    });
+
+    // Función para validar el captcha
+    function validateCaptcha() {
+        var userAnswer = parseInt(document.getElementById('captcha').value);
+        var correctSum = parseInt(document.getElementById('captcha').dataset.correctSum);
+
+        return userAnswer === correctSum;
+    }
+
+    // Función para mostrar mensajes de error del captcha
+    function displayCaptchaError() {
+        validationMessages.innerHTML = '<p class="validation-item">Captcha incorrecto. Por favor, vuelva a intentarlo.</p>';
+
+    }
+});
